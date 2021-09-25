5d22ecaf8d8f1b545e7e51187d2bff20b21bf29e
Santeri Salonen <santerisa.salonen@gmail.com>
2021-09-25 16:13:24 +0300

    New models, new testing for code

--- src/app.py
+++ src/app.py
@@ -1,55 +1,149 @@
 import random
 
-class AI:
+class Models:
     '''Class that controls the AI and it's decisions'''
 
+    def __init__(self, memory):
+        self.memory = memory
+
+    def model_0(self):
+        '''Model 0: Chooses what would've lost to player's previous choice.'''
+        last = self.memory.get_result()
+        if len(last) == 0:
+            self.m0_pick = random.randint(1, 3)
+        else:
+            self.m0_pick = last[-1][0]
+            if self.m0_pick == 1:
+                self.m0_pick += 2
+            else:
+                self.m0_pick -= 1
+    
+    def model_1(self):
+        '''Model 1: Chooses what would've won player's previous choice.'''
+        last = self.memory.get_result()
+        self.m1_pick = last[-1][0]
+        if self.m1_pick == 3:
+            self.m1_pick = 1
+        else:
+            self.m1_pick += 1
+
+    def model_2(self):
+        '''Model 2: Chooses what would've tied with player's previous choice.'''
+        last = self.memory.get_result()
+        self.m2_pick = last[-1][0]
+
+    #TODO: Vector model: player plays in certain pattern rock-scissors, scissors-paper, paper-rock, vector = +2
+        #Least frequent model: beat players least frequent pick
+        
+    def get_models(self):
+        '''Returns all modelpicks to the Ensembler'''
+        last = self.memory.get_result()
+        self.model_0()
+        if len(last) == 0:
+            return [self.m0_pick]
+        self.model_1()
+        self.model_2()
+        return (self.m0_pick, self.m1_pick, self.m2_pick)
+        
+
+class Ensembler:
+    '''Ensembler chooses the best model for all situations.'''
+    def __init__(self, models):
+        '''Ensembler constructor
+
+        Args:
+            models: class that holds different models for the Ensembler to decide which to use.'''
+
+        self.models = models
+        self.round = 0
+
+        #Model scores:
+        self.scores = [0, 0, 0]
+
+    def pick(self):
+        '''Decides which model the Ensembler should pick.
+        First round plays random, otherwise checks the scores of different models.
+        
+        Returns:
+            Ensembler's pick
+        '''
+        model_pick = self.models.get_models()
+        if self.round == 0:
+            pick = model_pick[0]
+        else:
+            model = self.scores.index(max(self.scores))
+            pick = model_pick[model]
+        self.round += 1
+        return pick
+
+    def add_score(self, player_pick):
+        '''Keeps scoring for every model, ensembler uses the scores to play against the player.
+        Gives score to all models depending on if they choose wrong or right.
+        #TODO: Ensembler should only remember the last 5 rounds.
+        
+        Args:
+            player_pick: What player picked this round. Compares this to models' picks.
+        '''
+
+        model_pick = self.models.get_models()
+        if player_pick == 3:
+            correct_pick = 1
+        else:
+            correct_pick = player_pick + 1
+        for i in model_pick:
+            if i == correct_pick:
+                ind = model_pick.index(i)
+                self.scores[ind] += 1
+            if i != correct_pick and i != player_pick:
+                ind = model_pick.index(i)
+                self.scores[ind] -= 1
+
+
+
+class Memory:
+    '''Memory to remember earlier rounds.'''
     def __init__(self):
         self.last_5_rounds = []
 
-    def pick(self):
-        '''Decides what the AI should do.
-
-        Currently on round 1: pick random,
-        other rounds: pick what would lose to player's last pick
-        
-        Returns:
-            AI's pick
-        '''
-        if len(self.last_5_rounds) == 0:
-            return random.randint(1, 3)
-        else:
-            ai_pick = self.last_5_rounds[-1][0]
-            if ai_pick == 1:
-                ai_pick += 2
-            else: ai_pick -= 1
-            return ai_pick
-
     def add_result(self, player_pick, ai_pick, result):
-        '''Adds a result to AI's memory for later use and learning.
+        '''Adds a result to memory for later use and learning.
         Memory is kept only 5 rounds long, later rounds will be removed.
+        Picks are stored as a number value:
+            1 = Rock, 2 = Paper, 3 = Scissors
+        Results are stored in numbers as well:
+            0 = Tie, 1 = AI Wins, 2 = Player Wins
         '''
 
-        if len(self.last_5_rounds) <= 5:
+        if len(self.last_5_rounds) < 5:
             self.last_5_rounds.append((player_pick, ai_pick, result))
         else:
             self.last_5_rounds.pop(0)
             self.last_5_rounds.append((player_pick, ai_pick, result))
+        
+
+    def get_result(self):
+        return self.last_5_rounds
+
 
 
 class RPS:
-    '''Creates the fountation for the game'''
-    def __init__(self, ai):
-        '''Rock-paper-scissors game's constructor
+    '''Creates the fountation for the game.'''
+    def __init__(self, ensembler, memory):
+        '''Rock-paper-scissors gameconstructor
         
         Args:
-            ai: AI-class to be used in the game'''
+            ai: Ensembler that will use the right model to defeat the player.
+            memory: class to store previous rounds.'''
 
-        self.ai = ai
-        self.round = 1
+        self.ai = ensembler
+        self.memory = memory
+        self.round = 0
 
     def calculate(self, player_pick):
         '''Asks the AI what it picks for this round using the method in AI-class.
         Combines it with the players pick to send it to pick_winner to get the result.
+        add_score: Ensembler scores all models depending on the result.
+        add_result: Adds outcome to Memory
         
         Args:
             player_pick: What the player has picked:
@@ -61,10 +155,13 @@ class RPS:
         '''
 
         ai_pick = self.ai.pick()
-        self.round += 1
+        if self.round != 0:
+            self.ai.add_score(player_pick)
         result = self.pick_winner(player_pick, ai_pick)
-        self.ai.add_result(player_pick, ai_pick, result)
-        return result
+        self.memory.add_result(player_pick, ai_pick, result)
+
+        self.round += 1
+        return result, ai_pick
 
 
     def pick_winner(self, player_pick, ai_pick):
@@ -79,11 +176,8 @@ class RPS:
             result: If AI or the player won or they tied:
                 0 = Tie, 1 = AI Wins, 2 = Player Wins
         '''
-        if player_pick == 1 and ai_pick == 1:
-            return 0
-        if player_pick == 2 and ai_pick == 2:
-            return 0
-        if player_pick == 3 and ai_pick == 3:
+        #Ties:
+        if player_pick == ai_pick:
             return 0
 
         #AI wins:
@@ -102,10 +196,13 @@ class RPS:
         if player_pick == 3 and ai_pick == 2:
             return 2
 
+
 '''
 #Temporary system to run the game on the console:
-ai = AI()
-machine = RPS(ai)
+mem = Memory()
+ai = Models(mem)
+ensembler = Ensembler(ai)
+machine = RPS(ensembler, mem)
 print("1 = Rock")
 print("2 = Paper")
 print("3 = Scissors")
@@ -119,17 +216,27 @@ while True:
     if player_pick == 0: break
 
     result = machine.calculate(player_pick)
-    if result == 0:
+    if result[0] == 0:
         print("It's a tie!")
         ties += 1
-    if result == 1:
+    if result[0] == 1:
         print("AI wins!")
         ai_wins += 1
-    if result == 2:
+    if result[0] == 2:
         print("You win!")
         player_wins += 1
-    
+
+    if result[1] == 1:
+        print("AI's pick: Rock")
+    if result[1] == 2:
+        print("AI's pick: Paper")
+    if result[1] == 3:
+        print("AI's pick: Scissors")
+
+
+
     print(f"Total: W:{player_wins} T:{ties} L:{ai_wins}")
+    print(ensembler.scores)
     print("")
 '''
 
--- src/tests/app_test.py
+++ src/tests/app_test.py
@@ -1,34 +1,122 @@
 import unittest
-from app import RPS, AI
+from app import Models, Ensembler, Memory, RPS
 
 
-class TestAI(unittest.TestCase):
+class TestModels(unittest.TestCase):
     def setUp(self):
-        self.ai = AI()
-        self.game = RPS(self.ai)
+        self.mem = Memory()
+        self.models = Models(self.mem)
+        self.ai = Ensembler(self.models)
+        self.game = RPS(self.ai, self.mem)
 
-    def test_constructor_creates_list(self):
-        '''Test if AI-class creates a list'''
+    def test_model_0(self):
+        '''Tests that model 0 picks correct.'''
+        self.game.calculate(1)
+        self.models.model_0()
+        pick = self.models.m0_pick
+        self.assertEqual(pick, 3)
 
-        self.assertEqual(self.ai.last_5_rounds, [])
+    def test_model_1(self):
+        '''Tests that model 1 picks correct.'''
+        self.game.calculate(1)
+        self.models.model_1()
+        pick = self.models.m1_pick
+        self.assertEqual(pick, 2)
+
+    def test_model_2(self):
+        '''Tests that model 2 picks correct.'''
+        self.game.calculate(1)
+        self.models.model_2()
+        pick = self.models.m2_pick
+        self.assertEqual(pick, 1)
+
+    def test_model_output(self):
+        '''Tests get_models output.'''
+        self.game.calculate(1)
+        self.game.calculate(1)
+        output = self.models.get_models()
+        self.assertEqual(output, (3, 2, 1))
+
+
+class TestEnsembler(unittest.TestCase):
+    def setUp(self):
+        self.mem = Memory()
+        self.models = Models(self.mem)
+        self.ai = Ensembler(self.models)
+        self.game = RPS(self.ai, self.mem)
     
-    def test_ai_picks_correct_short(self):
-        '''Test if AI picks scissors after player played rock the previous round, short game'''
+    def test_ai_wins_same_pick(self):
+        '''If player only picks the same pick, the AI will defeat the player.'''
         self.game.calculate(1)
         self.game.calculate(1)
-        self.assertEqual(self.ai.pick(), 3)
-
-    def test_ai_picks_correct_long(self):
-        '''Test if AI picks scissors after player played rock the previous round, long game'''
-        for i in range(100):
-            self.game.calculate(3)
+        self.game.calculate(1)
+        self.game.calculate(1)
+        self.game.calculate(1)
+        self.game.calculate(1)
         self.assertEqual(self.ai.pick(), 2)
+
+    def test_ai_wins_repeated_pick(self):
+        '''If player repeats the same picks, the AI will defeat the player.'''
+        self.game.calculate(1)
+        self.game.calculate(2)
+        self.game.calculate(3)
+        self.game.calculate(1)
+        self.game.calculate(2)
+        self.game.calculate(3)
+        self.game.calculate(1)
+        self.game.calculate(2)
+        self.game.calculate(3)
+        self.game.calculate(1)
+        self.game.calculate(2)
+        self.game.calculate(3)
+        self.assertEqual(self.ai.pick(), 2)
+
+    def test_correct_score_addition(self):
+        '''Tests that Ensembler adds scores correctly.'''
+        self.game.calculate(1)
+        self.game.calculate(2)
+        self.game.calculate(3)
+        self.game.calculate(1)
+        self.game.calculate(2)
+        m0_score = self.ai.scores[0]
+        m1_score = self.ai.scores[1]
+        m2_score = self.ai.scores[2]
+        self.assertTrue(m0_score < 5 and m1_score < 5 and m2_score < 5)
+
+
+class TestMemory(unittest.TestCase):
+    def setUp(self):
+        self.mem = Memory()
+    
+    def test_correct_list_addition_short(self):
+        '''Tests that list adds correctly.'''
+        self.mem.add_result(1, 1, 0)
+        self.assertEqual(len(self.mem.last_5_rounds), 1)
+    
+    def test_correct_list_addition_long(self):
+        '''Tests that list doesn't exceed 5 values.'''
+        self.mem.add_result(1, 1, 0)
+        self.mem.add_result(1, 1, 0)
+        self.mem.add_result(1, 1, 0)
+        self.mem.add_result(1, 1, 0)
+        self.mem.add_result(1, 1, 0)
+        self.mem.add_result(1, 1, 0)
+        self.mem.add_result(1, 1, 0)
+        self.assertEqual(len(self.mem.last_5_rounds), 5)
+
+    def test_correct_return(self):
+        '''Tests that get_result works correctly.'''
+        self.mem.add_result(1, 1, 0)
+        result = self.mem.get_result()
+        self.assertEqual(result, [(1, 1, 0)])
 
 
 class TestRPS(unittest.TestCase):
     def setUp(self):
-        self.ai = AI()
-        self.game = RPS(self.ai)
+        self.mem = Memory()
+        self.models = Models(self.mem)
+        self.ai = Ensembler(self.models)
+        self.game = RPS(self.ai, self.mem)
 
     def test_correct_winner(self):
         '''Test if RPS picks right winner (0 = Tie)'''
@@ -37,10 +125,7 @@ class TestRPS(unittest.TestCase):
         self.assertEqual(result, 0)
 
     def test_correct_calculation(self):
-        '''Test if RPS calculates the right winner.
-        AI currently picks the one that would lose to players previous pick.
-        Player should win.
-        '''
+        '''Test if RPS calculation returns correct values.'''
         self.game.calculate(2)
-        result = self.game.calculate(2)
-        self.assertEqual(result, 2)
+        result = self.game.calculate(2)[0]
+        self.assertTrue(result == 0 or result == 1 or result == 2)
