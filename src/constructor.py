154fd43d6e11cae6708b8582e2592433c1811a6a
Santeri Salonen <santerisa.salonen@gmail.com>
2021-10-16 17:46:41 +0300

    Fixing Heroku

--- src/constructor.py
+++ src/constructor.py
@@ -6,6 +6,7 @@ class Constructor:
         self.models = Models(self.mem)
         self.ensembler = Ensembler(self.models)
         self.game = RPS(self.ensembler, self.mem)
+        self.score = [0, 0, 0]
 
     def play(self, input):
         '''Returns:
@@ -13,14 +14,18 @@ class Constructor:
                     result[1]: 1 = ai picked rock, 2 = ai picked paper, 3 = ai picked scissors
         '''
         result = self.game.calculate(input)
+        self.score[result[0]] += 1
         return result
 
-    def get_ai_score():
+    def get_ai_score(self):
         '''Returns:
             Scores of all models, index = model.'''
         return self.ensembler.get_score()
 
-    def get_ai_modelpicks():
+    def get_ai_modelpicks(self):
         '''Returns:
             What different models will pick for next round.'''
         return self.models.get_models()
+
+    def get_score(self):
+        return self.score
--- src/routes.py
+++ src/routes.py
@@ -2,43 +2,36 @@ from app import app
 from constructor import Constructor
 from flask import redirect, render_template, request, session, abort
 
-class Data():
-    constructor = Constructor()
-    output = None
-    score = [0, 0, 0]
-def add_score():
-    result = data.output[0]
-    data.score[result] += 1
-
-data = Data()
+class Data:
+    result = None
+    game = None
 
 @app.route("/")
 def index():
-    output = data.output
-    score = data.score
-    return render_template("index.html", output=output, score=score)
-
-@app.route("/rock", methods=["POST"])
-def rock():
-    data.output = data.constructor.play(1)
-    add_score()
-    return redirect("/")
-
-@app.route("/paper", methods=["POST"])
-def paper():
-    data.output = data.constructor.play(2)
-    add_score()
-    return redirect("/")
-
-@app.route("/scissors", methods=["POST"])
-def scissors():
-    data.output = data.constructor.play(3)
-    add_score()
-    return redirect("/")
+    return render_template("index.html")
 
 @app.route("/reset", methods=["POST"])
 def reset():
-    data.constructor = Constructor()
-    data.output = None
-    data.score = [0, 0, 0]
-    return redirect("/")
+    Data.game = Constructor()
+    return redirect("/rps")
+
+@app.route("/rps")
+def rps():
+    score = Data.game.get_score()
+    output = Data.result
+    return render_template("rps.html", output=output, score=score)
+
+@app.route("/rock", methods=["POST"])
+def rock():
+    Data.result = Data.game.play(1)
+    return redirect("/rps")
+
+@app.route("/paper", methods=["POST"])
+def paper():
+    Data.result = Data.game.play(2)
+    return redirect("/rps")
+
+@app.route("/scissors", methods=["POST"])
+def scissors():
+    Data.result = Data.game.play(3)
+    return redirect("/rps")
